691b1ff692fed720529f77659ed1d9b89cee64f7
Emyr298 <emi.ru298@gmail.com>
2025-02-02 09:30:44 +0700

    refactor: use strategy pattern for source

--- src/main/java/id/ac/ui/cs/netlog/StreamingJob.java
+++ src/main/java/id/ac/ui/cs/netlog/StreamingJob.java
@@ -1,11 +1,8 @@
 package id.ac.ui.cs.netlog;
 
-import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.connector.base.DeliveryGuarantee;
 import org.apache.flink.connector.kafka.sink.KafkaSink;
-import org.apache.flink.connector.kafka.source.KafkaSource;
-import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSink;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -13,40 +10,32 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import id.ac.ui.cs.netlog.data.cicflowmeter.FlowStats;
-import id.ac.ui.cs.netlog.data.packets.Packet;
 import id.ac.ui.cs.netlog.operators.ExtractFlowStats;
 import id.ac.ui.cs.netlog.operators.ExtractPacketInfo;
 import id.ac.ui.cs.netlog.operators.FlowGenerator;
-import id.ac.ui.cs.netlog.operators.PacketParser;
-import id.ac.ui.cs.netlog.serialization.deserializers.PacketDeserializationSchema;
 import id.ac.ui.cs.netlog.serialization.serializers.ObjectSerializer;
+import id.ac.ui.cs.netlog.source.StreamSource;
+import id.ac.ui.cs.netlog.source.enums.StreamSourceEnum;
+import id.ac.ui.cs.netlog.source.factory.StreamSourceFactory;
 
 public class StreamingJob {
 	private static final String SOURCE_TYPE = "source";
-	private static final String SOURCE_KAFKA_SERVERS = "source-servers";
-	private static final String SOURCE_KAFKA_TOPIC = "source-topic";
-	private static final String SOURCE_GROUP_ID = "source-group";
-	private static final String SOURCE_SOCKET_HOST = "source-host";
-	private static final String SOURCE_SOCKET_PORT = "source-port";
 
 	private static final String SINK_TYPE = "sink";
 	private static final String SINK_KAFKA_SERVERS = "sink-servers";
 	private static final String SINK_KAFKA_TOPIC = "sink-topic";
-	
 
     public static void main(String[] args) throws Exception {
 		ObjectMapper objectMapper = new ObjectMapper();
 		ParameterTool parameters = ParameterTool.fromArgs(args);
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-		DataStream<Packet> source;
-        if (parameters.get(SOURCE_TYPE, "socket").equals("kafka")) {
-			source = getKafkaSource(env, parameters, objectMapper);
-		} else {
-			source = getSocketSource(env, parameters, objectMapper);
-		}
+		String sourceString = parameters.get(SOURCE_TYPE, StreamSourceEnum.SOCKET.toString());
 
-        DataStream<FlowStats> stream = source
+		StreamSourceFactory factory = new StreamSourceFactory(objectMapper);
+		StreamSource source = factory.getStreamSource(StreamSourceEnum.fromString(sourceString), env, parameters);
+
+        DataStream<FlowStats> stream = source.getSourceStream()
 				.map(new ExtractPacketInfo())
 				.keyBy(packetInfo -> packetInfo.getFlowBidirectionalId())
 				.process(new FlowGenerator(true))
@@ -60,29 +49,6 @@ public class StreamingJob {
 
 		env.execute("FlinkFlowMeter");
     }
-
-	private static DataStream<Packet> getKafkaSource(StreamExecutionEnvironment env, ParameterTool parameters, ObjectMapper objectMapper) {
-		String servers = parameters.get(SOURCE_KAFKA_SERVERS, "localhost:9200");
-		String topic = parameters.get(SOURCE_KAFKA_TOPIC, "network-traffic");
-		String groupId = parameters.get(SOURCE_GROUP_ID, "flink-1");
-
-		KafkaSource<Packet> source = KafkaSource.<Packet>builder()
-			.setBootstrapServers(servers)
-			.setTopics(topic)
-			.setGroupId(groupId)
-			.setStartingOffsets(OffsetsInitializer.earliest())
-			.setValueOnlyDeserializer(new PacketDeserializationSchema(objectMapper))
-			.build();
-
-		return env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source");
-	}
-
-	private static DataStream<Packet> getSocketSource(StreamExecutionEnvironment env, ParameterTool parameters, ObjectMapper objectMapper) {
-		String host = parameters.get(SOURCE_SOCKET_HOST, "host.docker.internal");
-		Integer port = Integer.parseInt(parameters.get(SOURCE_SOCKET_PORT, "9999"));
-
-		return env.socketTextStream(host, port).map(new PacketParser(objectMapper));
-	}
 
 	private static DataStreamSink<FlowStats> printSink(DataStream<FlowStats> stream) {
 		return stream.print();
--- src/main/java/id/ac/ui/cs/netlog/source/enums/StreamSourceEnum.java
+++ src/main/java/id/ac/ui/cs/netlog/source/enums/StreamSourceEnum.java
@@ -1,6 +1,33 @@
 package id.ac.ui.cs.netlog.source.enums;
 
 public enum StreamSourceEnum {
-    KAFKA,
-    SOCKET;
+    KAFKA("kafka"),
+    SOCKET("socket");
+
+    private final String value;
+
+    StreamSourceEnum(String value) {
+        this.value = value;
+    }
+
+    public static StreamSourceEnum fromString(String text) {
+        if (text == null) {
+            throw new IllegalArgumentException("String value cannot be null");
+        }
+
+        for (StreamSourceEnum status : StreamSourceEnum.values()) {
+            if (status.value.equalsIgnoreCase(text)) {
+                return status;
+            }
+        }
+
+        throw new IllegalArgumentException(
+            String.format("No constant with text '%s' found", text)
+        );
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
 }
