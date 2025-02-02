fb8564ecc0ed5481923b964035b54c7fcb6ea1b2
Emyr298 <emi.ru298@gmail.com>
2025-02-02 09:07:23 +0700

    feat: add configurable sink

--- src/main/java/id/ac/ui/cs/netlog/StreamingJob.java
+++ src/main/java/id/ac/ui/cs/netlog/StreamingJob.java
@@ -2,9 +2,12 @@ package id.ac.ui.cs.netlog;
 
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.connector.base.DeliveryGuarantee;
+import org.apache.flink.connector.kafka.sink.KafkaSink;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSink;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -16,14 +19,19 @@ import id.ac.ui.cs.netlog.operators.ExtractPacketInfo;
 import id.ac.ui.cs.netlog.operators.FlowGenerator;
 import id.ac.ui.cs.netlog.operators.PacketParser;
 import id.ac.ui.cs.netlog.serialization.deserializers.PacketDeserializationSchema;
+import id.ac.ui.cs.netlog.serialization.serializers.ObjectSerializer;
 
 public class StreamingJob {
 	private static final String SOURCE_TYPE = "source";
-	private static final String SOURCE_KAFKA_SERVERS = "servers";
-	private static final String SOURCE_KAFKA_TOPIC = "topic";
-	private static final String SOURCE_GROUP_ID = "group";
-	private static final String SOURCE_SOCKET_HOST = "host";
-	private static final String SOURCE_SOCKET_PORT = "port";
+	private static final String SOURCE_KAFKA_SERVERS = "source-servers";
+	private static final String SOURCE_KAFKA_TOPIC = "source-topic";
+	private static final String SOURCE_GROUP_ID = "source-group";
+	private static final String SOURCE_SOCKET_HOST = "source-host";
+	private static final String SOURCE_SOCKET_PORT = "source-port";
+
+	private static final String SINK_TYPE = "sink";
+	private static final String SINK_KAFKA_SERVERS = "sink-servers";
+	private static final String SINK_KAFKA_TOPIC = "sink-topic";
 	
 
     public static void main(String[] args) throws Exception {
@@ -43,14 +51,19 @@ public class StreamingJob {
 				.keyBy(packetInfo -> packetInfo.getFlowBidirectionalId())
 				.process(new FlowGenerator(true))
 				.map(new ExtractFlowStats());
-				stream.print();
+		
+		if (parameters.get(SINK_TYPE, "print").equals("kafka")) {
+			kafkaSink(parameters, stream, objectMapper);
+		} else {
+			printSink(stream);
+		}
 
 		env.execute("FlinkFlowMeter");
     }
 
 	private static DataStream<Packet> getKafkaSource(StreamExecutionEnvironment env, ParameterTool parameters, ObjectMapper objectMapper) {
 		String servers = parameters.get(SOURCE_KAFKA_SERVERS, "localhost:9200");
-		String topic = parameters.get(SOURCE_KAFKA_TOPIC, "network-flows");
+		String topic = parameters.get(SOURCE_KAFKA_TOPIC, "network-traffic");
 		String groupId = parameters.get(SOURCE_GROUP_ID, "flink-1");
 
 		KafkaSource<Packet> source = KafkaSource.<Packet>builder()
@@ -69,5 +82,21 @@ public class StreamingJob {
 		Integer port = Integer.parseInt(parameters.get(SOURCE_SOCKET_PORT, "9999"));
 
 		return env.socketTextStream(host, port).map(new PacketParser(objectMapper));
+	}
+
+	private static DataStreamSink<FlowStats> printSink(DataStream<FlowStats> stream) {
+		return stream.print();
+	}
+
+	private static DataStreamSink<FlowStats> kafkaSink(ParameterTool parameters, DataStream<FlowStats> stream, ObjectMapper objectMapper) {
+		String servers = parameters.get(SINK_KAFKA_SERVERS, "localhost:9200");
+		String topic = parameters.get(SINK_KAFKA_TOPIC, "network-flows");
+
+		KafkaSink<FlowStats> sink = KafkaSink.<FlowStats>builder()
+			.setBootstrapServers(servers)
+			.setRecordSerializer(new ObjectSerializer<FlowStats>(topic, objectMapper))
+			.setDeliveryGuarantee(DeliveryGuarantee.AT_LEAST_ONCE)
+			.build();
+		return stream.sinkTo(sink);
 	}
 }
