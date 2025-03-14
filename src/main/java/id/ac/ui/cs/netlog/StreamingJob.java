ef35ee463296a7644d461dba0b77a1b612c20ccc
Emyr298 <emi.ru298@gmail.com>
2025-03-14 22:25:04 +0700

    feat: mode parameter

--- src/main/java/id/ac/ui/cs/netlog/StreamingJob.java
+++ src/main/java/id/ac/ui/cs/netlog/StreamingJob.java
@@ -1,7 +1,5 @@
 package id.ac.ui.cs.netlog;
 
-import java.util.List;
-
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.connector.base.DeliveryGuarantee;
 import org.apache.flink.connector.kafka.sink.KafkaSink;
@@ -12,12 +10,9 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import id.ac.ui.cs.netlog.data.cicflowmeter.FlowStats;
-import id.ac.ui.cs.netlog.data.cicflowmeter.PacketInfo;
-import id.ac.ui.cs.netlog.operators.ExtractFlowStats;
-import id.ac.ui.cs.netlog.operators.ExtractPacketInfo;
-import id.ac.ui.cs.netlog.operators.FlowGenerator;
-import id.ac.ui.cs.netlog.operators.OrderPackets;
-import id.ac.ui.cs.netlog.operators.OrderPacketsDebugger;
+import id.ac.ui.cs.netlog.modes.StreamMode;
+import id.ac.ui.cs.netlog.modes.enums.ModeEnum;
+import id.ac.ui.cs.netlog.modes.factory.ModeFactory;
 import id.ac.ui.cs.netlog.serialization.serializers.ObjectSerializer;
 import id.ac.ui.cs.netlog.source.StreamSource;
 import id.ac.ui.cs.netlog.source.enums.StreamSourceEnum;
@@ -25,6 +20,8 @@ import id.ac.ui.cs.netlog.source.factory.StreamSourceFactory;
 
 public class StreamingJob {
 	private static final String SOURCE_TYPE = "source";
+
+	private static final String MODE = "mode";
 
 	private static final String SINK_TYPE = "sink";
 	private static final String SINK_KAFKA_SERVERS = "sink-servers";
@@ -36,38 +33,22 @@ public class StreamingJob {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
 		String sourceString = parameters.get(SOURCE_TYPE, StreamSourceEnum.SOCKET.toString());
-		System.out.println(sourceString);
 
-		StreamSourceFactory factory = new StreamSourceFactory(objectMapper);
-		StreamSource source = factory.getStreamSource(StreamSourceEnum.fromString(sourceString), env, parameters);
+		StreamSourceFactory sourceFactory = new StreamSourceFactory(objectMapper);
+		StreamSource source = sourceFactory.getStreamSource(StreamSourceEnum.fromString(sourceString), env, parameters);
 
-        // DataStream<FlowStats> stream = source.getSourceStream()
-		// 		.map(new ExtractPacketInfo())
-		// 		.keyBy(packetInfo -> {
-		// 			String id = packetInfo.getFlowBidirectionalId();
-		// 			System.out.println(id);
-		// 			return id;
-		// 		})
-		// 		.process(new FlowGenerator(true))
-		// 		.map(new ExtractFlowStats());
+		String modeString = parameters.get(MODE, ModeEnum.ORDERED.toString());
 
-		DataStream<List<PacketInfo>> stream = source.getSourceStream()
-				.map(new ExtractPacketInfo())
-				.keyBy(packetInfo -> {
-					String id = packetInfo.getPublisherId() + "-" + packetInfo.getFlowBidirectionalId();
-					System.out.println(id);
-					return id;
-				})
-				.process(new OrderPackets())
-				.flatMap(new OrderPacketsDebugger());
-		
-		stream.print();
+		ModeFactory modeFactory = new ModeFactory();
+		StreamMode streamMode = modeFactory.getMode(ModeEnum.fromString(modeString));
 
-		// if (parameters.get(SINK_TYPE, "print").equals("kafka")) {
-		// 	kafkaSink(parameters, stream, objectMapper);
-		// } else {
-		// 	printSink(stream);
-		// }
+		DataStream<FlowStats> stream = streamMode.createPipeline(source);
+
+		if (parameters.get(SINK_TYPE, "print").equals("kafka")) {
+			kafkaSink(parameters, stream, objectMapper);
+		} else {
+			printSink(stream);
+		}
 
 		env.execute("FlinkFlowMeter");
     }
